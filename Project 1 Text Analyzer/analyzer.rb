68ae1d2658934fc23b5061109bf06e3589a0440c
tanya selvog <tanyaSelvog@gmail.com>
2019-01-28 13:50:40 -0800

    added 'calculating averages'

--- Project 1 Text Analyzer/analyzer.rb
+++ Project 1 Text Analyzer/analyzer.rb
@@ -12,3 +12,21 @@ puts "#{total_characters} characters"
 
 total_characters_nospaces = text.gsub(/\s+/, '').length
 puts "#{total_characters_nospaces} characters excluding spaces"
+
+
+#counting words
+word_count = text.split.length
+puts "#{word_count} words"
+
+#counting paragraphs and sentences
+
+paragraph_count = text.split(/\n\n/).length
+puts "#{paragraph_count} paragraphs"
+
+sentence_count = text.split(/\.|\?|!/).length
+puts "#{sentence_count} sentences"
+
+
+#calculating averages
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
