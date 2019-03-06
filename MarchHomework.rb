0a35d89df27ed523c732544a294524242fe26add
tanya selvog <tanyaSelvog@gmail.com>
2019-03-05 17:03:53 -0800

    started 3/5/19 work - created 3 classes

--- MarchHomework.rb
+++ MarchHomework.rb
@@ -26,3 +26,27 @@ animals = [Cat.new("Sparrow"), Dog.new("Ada"), Cat.new("Neffie")]
 animals.each do |animal|
     puts animal.talk
 end
+
+
+# 3/5/19
+
+class Animal
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+class Cow < Animal
+    def talk
+        "Moo"
+    end
+end
+
+class Sheep < Animal
+    def talk
+        "Baaa"
+    end
+end
+
